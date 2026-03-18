a8d32efcb7b22cf62e7f13786f5da9fe7d6cbb90
Mikayla <151647406+emgrew24@users.noreply.github.com>
2026-03-18 19:56:16 -0400

    Comments added

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
-// const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-// Website link if hosting from node: http://localhost:5959/
-// Website link if hosting from github: https://emgrew24.github.io/gamerdailies.github.io/
-// Website link from hosting on Vercel: https://gamer-dailies.vercel.app/
+// Website link hosted on Vercel: https://gamer-dailies.vercel.app/
+
+
+// Connecting the index.js to Vercel so it will launch from here 
+// instead of directly from the html to allow the routing.
 
 
 // Creating the server (second attempt with some research)
@@ -39,45 +40,3 @@ module.exports = (req, res)=>{
     }
 
 }; 
-
-
-
-
-
-
-// // Creating the server (first attempt -- not working with github/Vercel)
-// const server = http.createServer( (req, res)=>{
-
-//     // Main landing page
-//     if(req.url === '/'){
-//         fs.readFile( 
-//             path.join(__dirname, 'index.html'),
-//                 (err, content)=>{
-//                     if(err) throw err;
-
-//                     res.writeHead(200,{'Content-Type': 'text/html'});
-//                     res.end(content);
-//                 });
-//     }
-//     // Redirects the user to the json database 
-//     else if (req.url === '/api'){
-//         fs.readFile(
-//             path.join(__dirname, 'db.json'), 'utf-8',
-//                     (err, content) => {
-//                         if (err) throw err;
-
-//                         res.writeHead(200, {'Content-Type': 'application/json'});
-//                         res.end(content);
-//                     }
-//         );
-//     }
-//     else{
-//         res.end("<h1> Nothing is here </h1>");
-//     }
-
-// }); 
-
-
-// const PORT = process.env.PORT || 5959;
-
-// server.listen(PORT, ()=> console.log('Server is running on port: ' +PORT));
